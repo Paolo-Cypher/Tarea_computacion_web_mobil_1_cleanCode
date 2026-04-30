e63e3b9079d87b85799d2efc04f6e6441c4bcf39
Bastian Encina <drakefirst09@gmail.com>
2026-04-29 20:20:46 -0400

    Reformulando comentarios.

--- src/utils/DevOps/Paginacion.js
+++ src/utils/DevOps/Paginacion.js
@@ -1,11 +1,11 @@
 //Las tres funciones en una sola.
 export function paginate(items, page, size){
-  const start = (page - 1) * size;
-  return{
+  const start = (page - 1) * size; // iniciar pagina en 0.
+  return{ // retorno del objeto de pagiacion.
     items: items.slice(start, start + size),
-    page,
-    total: items.length,
-    totalPages: Math.ceil(items.length / size),
-    size
+    page,//indica la pagina actual.
+    total: items.length, // cantidad de elementos antes de dividirlo.
+    totalPages: Math.ceil(items.length / size),// calculo total de paginas.
+    size // Esto es para saber cuantos elementos se solicitaron por pagina.
   };
 }
